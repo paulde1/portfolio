5fe8782aecba0a667dc6c3d2f41aefe34e41d94e
Paul De Cunha <pauldecunha7117@gmail.com>
2023-01-25 20:11:52 -0500

    about section almost complete

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -3,26 +3,26 @@ import { motion } from 'framer-motion';
 
 // import { AppWrap, MotionWrap } from '../../wrapper';
 import './About.scss';
-// import { urlFor, client } from '../../client';
+import { urlFor, client } from '../../client';
 import {images} from '../../constants';
 
 const About = () => {
-  // const [abouts, setAbouts] = useState([]);
+  const [abouts, setAbouts] = useState([]);
 
-  const abouts = [
-    {title: 'Fullstack Development', description: 'I am a fullstack developer',imgUrl: images.about01},
-    {title: 'Backend Development', description: 'I am a backend developer', imgUrl: images.about02},
-    {title: 'frontend Development', description: 'I am a frontend developer',imgUrl: images.about03},
-    {title: 'UI/UX  Development', description: 'I am a design developer',imgUrl: images.about04},
-  ]
+  // const abouts = [
+  //   {title: 'Fullstack Development', description: 'I am a fullstack developer',imgUrl: images.about01},
+  //   {title: 'Backend Development', description: 'I am a backend developer', imgUrl: images.about02},
+  //   {title: 'Frontend Development', description: 'I am a frontend developer',imgUrl: images.about03},
+  //   {title: 'MERN Stack', description: 'I am a design developer',imgUrl: images.about04},
+  // ]
 
-  // useEffect(() => {
-  //   const query = '*[_type == "abouts"]';
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
 
-  //   client.fetch(query).then((data) => {
-  //     setAbouts(data);
-  //   });
-  // }, []);
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, []);
 
   return (
     <>
@@ -37,8 +37,7 @@ const About = () => {
             className="app__profile-item"
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
-            {/* <img src={urlFor(about.imgUrl)} alt={about.title} /> */}
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
             <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
           </motion.div>

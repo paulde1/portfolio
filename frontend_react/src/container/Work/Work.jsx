e060da12af678b4d283ca09477f065ee6c16ae1d
Paul De Cunha <pauldecunha7117@gmail.com>
2023-03-01 16:21:51 -0500

    adjusted design

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -42,7 +42,7 @@ const Work = () => {
       </h2>
 
       <div className='app__work-filter'>
-        {["Web Application", "Mobile Application", "All"].map((item, index) => (
+        {["react", "Mobile Application", "All"].map((item, index) => (
           <div
             key={index}
             onClick={() => handleWorkFilter(item)}
